6c13056fcec73d8b387fc6c00b95059f87c1b1b1
User542Dte <User542Dte@DESKTOP-66UT898>
2024-05-27 00:17:31 +0500

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SetAllLightsGray()
+        {
+            btnRed.Background = Brushes.Gray;
+            btnYellow.Background = Brushes.Gray;
+            btnGreen.Background = Brushes.Gray;
+        }
+
+        private void btnRed_Click(object sender, RoutedEventArgs e)
+        {
+            SetAllLightsGray();
+            btnRed.Background = Brushes.Red;
+        }
+
+        private void btnYellow_Click(object sender, RoutedEventArgs e)
+        {
+            SetAllLightsGray();
+            btnYellow.Background = Brushes.Yellow;
+        }
+
+        private void btnGreen_Click(object sender, RoutedEventArgs e)
+        {
+            SetAllLightsGray();
+            btnGreen.Background = Brushes.Green;
+        }
+    }
+}
